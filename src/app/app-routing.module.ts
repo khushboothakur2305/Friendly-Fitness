7f4bc673a95ea7db1dcda856bfbe23400980dd10
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-07-09 23:40:31 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from "./welcome/welcome.component";
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TrainingComponent } from './auth/training/training.component';
+import { CurrentTrainingComponent } from './auth/training/current-training/current-training.component';
+import { NewTrainingComponent } from './auth/training/new-training/new-training.component';
+import { PastTrainingComponent } from './auth/training/past-training/past-training.component';
 
 const routes: Routes = [
   {path:'',component:WelcomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'training',component:TrainingComponent},
+  // {path:'current-training',component:CurrentTrainingComponent},
+  // {path:'new-training',component:NewTrainingComponent},
+  // {path:'past-training',component:PastTrainingComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModuleModule } from './Module/Material_Module/material-module/material-module.module';
+import { FormsModule } from "@angular/forms";
+
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './auth/training/training.component';
@@ -12,7 +14,6 @@ import { CurrentTrainingComponent } from './auth/training/current-training/curre
 import { NewTrainingComponent } from './auth/training/new-training/new-training.component';
 import { PastTrainingComponent } from './auth/training/past-training/past-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,15 +23,17 @@ import { WelcomeComponent } from './welcome/welcome.component';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MaterialModuleModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
